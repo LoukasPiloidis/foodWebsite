5c7a0c48876e8cb32a44806b46f1ffe14997ab00
salt-dev <loukasmalias@hotmail.com>
2021-11-27 14:46:23 +0100

    init commit

---
+++ admin/index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>Food Order - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+  </head>
+  <body>
+    <header class="menu wrapper">
+      Menu goes here
+</header>
+    <section class="main-content wrapper">
+      Main content goes here
+    </section>
+    <footer class="footer wrapper">
+      2021 All rights reserved, Food-Service. Developed by - Loukas Piloidis
+    </footer>
+  </body>
+</html>
