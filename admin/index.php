ebd6c3a5c9f6d56118156e3429faf170cdbcac01
salt-dev <loukasmalias@hotmail.com>
2021-11-27 15:12:02 +0100

    basic styling for admin page

--- admin/index.php
+++ admin/index.php
@@ -5,12 +5,18 @@
   </head>
   <body>
     <header class="menu wrapper">
-      Menu goes here
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Admin</a></li>
+        <li><a href="#">Category</a></li>
+        <li><a href="#">Food</a></li>
+        <li><a href="#">Order</a></li>
+      </ul>
 </header>
     <section class="main-content wrapper">
       Main content goes here
     </section>
-    <footer class="footer wrapper">
+    <footer class="footer">
       2021 All rights reserved, Food-Service. Developed by - Loukas Piloidis
     </footer>
   </body>
