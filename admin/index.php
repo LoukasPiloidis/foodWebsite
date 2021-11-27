361edf88e4e6657ff97afc7b8f9a545ea68c3a5b
salt-dev <loukasmalias@hotmail.com>
2021-11-27 15:29:47 +0100

    styling for index completed

--- admin/index.php
+++ admin/index.php
@@ -4,7 +4,7 @@
     <link rel="stylesheet" href="../css/admin.css">
   </head>
   <body>
-    <header class="menu wrapper">
+    <header class="menu">
       <ul>
         <li><a href="#">Home</a></li>
         <li><a href="#">Admin</a></li>
@@ -13,8 +13,26 @@
         <li><a href="#">Order</a></li>
       </ul>
 </header>
-    <section class="main-content wrapper">
-      Main content goes here
+    <section class="main-content">
+      <h1>Dashboard</h1>
+      <div class='main-content__main-div'>
+        <div class="col-4">
+          <h2>5</h2>
+          <p>categories</p>
+        </div>
+        <div class="col-4">
+          <h2>5</h2>
+          <p>categories</p>
+        </div>
+        <div class="col-4">
+          <h2>5</h2>
+          <p>categories</p>
+        </div>
+        <div class="col-4">
+          <h2>5</h2>
+          <p>categories</p>
+        </div>
+      </div>
     </section>
     <footer class="footer">
       2021 All rights reserved, Food-Service. Developed by - Loukas Piloidis
